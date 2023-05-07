de3f3b2ccc01a02517c1de1337615ba9d90eedc0
Inderprit Singh <singhinderprit1992@gmail.com>
2023-05-07 01:42:47 -0400

    updated comments

--- server/db/index.js
+++ server/db/index.js
@@ -1,33 +1,17 @@
 const { db } = require('./db');
 
-const { Post, User, UserPost } = require('./relationships');
+const { Post, User } = require('./relationships');
 
 const init = async () => {
     try {
         await db.authenticate();
-        // await db.query('DROP TABLE IF EXISTS animals'); //this command will drop a table if it exists
         await db.sync({ force: true });
-        // const firstMessage = await Post.create({
-        //     message: 'this is a test message',
-        // });
-        const secondMessage = await Post.create({
-            message: 'this is another test message',
-        });
-        const testUser = await User.create({
-            firstName: 'Riley',
-            lastName: 'gonzalez',
-        });
-
-        // secondMessage.setUser(testUser);
-
+        // await db.query('DROP TABLE IF EXISTS userposts'); //this command will drop a table if it exists and needs to be
+        // //executed AFTER you connect/sync to the database
         console.log('data base is connected');
-        // await db.close();
     } catch (error) {
         console.log(error);
     }
 };
 
 init();
-
-Post.belongsToMany(User, { through: UserPost });
-User.belongsToMany(Post, { through: UserPost });
--- server/db/relationships.js
+++ server/db/relationships.js
@@ -2,4 +2,7 @@ const { Post } = require('./Models/Post/Post');
 const { User } = require('./Models/User/User');
 const { UserPost } = require('./Models/UserPost/UserPost');
 
+// Post.belongsToMany(User, { through: UserPost });
+// User.belongsToMany(Post, { through: UserPost });
+
 module.exports = { User, Post, UserPost };
